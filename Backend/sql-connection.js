746c6284efcc68ce98c35ca2f43ac530d9a3d2f3
ecetsn <ece.tosun@sabanciuniv.edu>
2024-03-18 14:26:40 +0300

    SQL Connection is Established Using dotenv

--- Backend/sql-connection.js
+++ Backend/sql-connection.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql');
+require('dotenv').config(); // Load environment variables from .env file
+
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const connection = mysql.createConnection({
   host: 'aws-cs308.c5gk4os06v5f.eu-north-1.rds.amazonaws.com',
   port: '3306',
   user: 'admin',
-  password: 'cs308project',
+  password: DB_PASSWORD,
   database: 'CS308_MAIN_SQL_DATABASE'
 });
 
 connection.connect();
+
+module.exports = connection;
