2aa9e6bc9b2004ab08cd690544e37867f00f8d0d
aygungoktug <goktug.aygun@sabanciuniv.edu>
2024-03-22 15:09:33 +0300

    Changed Insert Logic (not completed)

--- Backend/Data Generation/PilotData.js
+++ Backend/Data Generation/PilotData.js
@@ -1,18 +1,18 @@
 const faker = require("faker");
 const mysql = require('mysql');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const h = require('../helper_functions')
 const { PilotClass, LanguageClass, insertPilot, insertLanguage } = require("../Models/Pilot");
 
 
 
-var mode = "insert"; // Possible values are 'insert' and 'delete' 
-var numberOfInstances = 4; // Use this variable to limit how many instances you want to insert
-
+var mode = "insert"; // Possible values are 'insert' and `delete' 
+var queryToComplete = 10; // Use this variable to limit how many instances you want to insert
+var completedQueries = 0;
 
 // MongoDB connection
-const uri = "mongodb+srv://cs308:cs308Test@test-db.1sxjsvf.mongodb.net/?retryWrites=true&w=majority&appName=test-db";
-mongoose.connect(uri);
+// const uri = "mongodb+srv://cs308:cs308Test@test-db.1sxjsvf.mongodb.net/?retryWrites=true&w=majority&appName=test-db";
+// mongoose.connect(uri);
 
 // MySQL Connection
 const MySQL_connection = mysql.createConnection({
@@ -26,6 +26,7 @@ MySQL_connection.connect();
 
 
 if (mode == "delete") {
+    queryToComplete = 1;
     // MySQL Delete Query
     var query = "DELETE FROM KnownLanguages;";
     MySQL_connection.query(query, (err, results) => {
@@ -41,22 +42,26 @@ if (mode == "delete") {
 
         console.log(results);
     });
-
+    completedQueries++;
 }
 else if (mode == "insert") {
     // MySQL Insert Queries
     const pilotQuery = 'INSERT INTO Pilots (pilot_id, name, age, gender, nationality, sen_level, veh_rest, max_range, email) VALUES (?,?,?,?,?,?,?,?,?)';
-    const languageQuery = 'INSERT INTO KnownLanguages (pilot_id, crew_id, language) VALUES (?,?,?)';
-
-    for (var i = 0; i < numberOfInstances; i++) {
+    const languageInsertQuery = 'INSERT INTO KnownLanguages (pilot_id, language) VALUES (?,?)';
+    const languageUpdateQuery = "UPDATE KnownLanguages SET pilot_id = ? where pilot_id IS NULL AND language = ?;"
+    var completedQueries = 0;
+    for (var i = 0; i < queryToComplete; i++) {
         // Const Possible Values
         const nationalities = ["English", "German", "French", "Turkish", "Spanish", "Indian", "Chinese", "Japanese", "Czech", "Hungarian", "Russian", "Italian", "American", "Mexican", "Japanese", "Ukrainian", "Mongolian", "Australian", "Austrian", "Belgian", "Swedish", "Finnish", "Norwegian", "Danish", "Dutch", "Swiss", "Portuguese", "Polish", "Irish", "Greek", "Canadian", "Brazilian", "Bulgarian", "Israeli", "Thai", "Vietnamese", "Norwegian", "Slovak", "Estonian", "Lithuanian", "Latvian", "Maltese", "Luxembourgish", "Icelandic"];
         const languages = ["English", "German", "French", "Turkish", "Spanish", "Chinese", "Japanese", "Czech", "Hungarian", "Russian", "Italian", "Swedish", "Finnish", "Norwegian", "Danish", "Dutch", "Portuguese", "Polish", "Irish", "Greek", "Thai", "Vietnamese", "Slovak", "Estonian", "Lithuanian", "Latvian", "Maltese", "Icelandic", "Arabic", "Hindi", "Korean", "Swahili", "Bengali", "Mandarin"];
 
         // Generate Random Data
         var pilotID = h.generateID("pilot");
-        var firstName = faker.name.firstName();
-        var lastName = faker.name.lastName();
+        // var pilotID = "P-00000";
+        // var firstName = faker.name.firstName();
+        var firstName = "deneme";
+        var lastName = "deneme";
+        // var lastName = faker.name.lastName();
         var name = firstName + " " + lastName;
         var age = h.getRandomInt(30, 50);
         var gender = ["M", "F"].at(h.getRandomInt(0, 2));
@@ -78,6 +83,10 @@ else if (mode == "insert") {
             languagesSpoken.push(randomLanguage);
         }
 
+        // console.log(name)
+        // console.log(pilotID)
+
+        // continue;
         parameters = [pilotID, name, age, gender, nationality, sen_level, veh_rest, maxRange, email];
 
         // Add Pilot to MySQL Database
@@ -86,35 +95,58 @@ else if (mode == "insert") {
                 console.error('Error executing query:', error);
                 return;
             }
-            console.log('Rows affected:', results.affectedRows);
+            else {
+                console.log(`Inserted pilot with pilot_id ${parameters[i]}`);
+                for (var k = 0; k < languagesSpoken.length; k++) {
+                    (function (k) {
+                        MySQL_connection.query(languageInsertQuery, [pilotID, languagesSpoken[k]], (error, results, fields) => {
+                            if (error) {
+                                console.error('Error executing query:', error);
+                                return;
+                            }
+                            console.log(`Inserted language ${languagesSpoken[k]} for pilot with pilot_id ${pilotID}`);
+                        });
+                    })(k);
+                }
+                completedQueries++;
+                if (completedQueries == queryToComplete) {
+                    // Close MySQL Connection
+                    MySQL_connection.end((err) => {
+                        if (err) {
+                            console.error('Error closing database connection:', err.stack);
+                            return;
+                        }
+                        else{console.log("MySQL Connection is closed.")}
+                
+                    });
+                }
+            }
         });
 
         // Add languages spoken to MySQL Database
         parameters = []
-        for (var j = 0; j < languagesSpoken.length; j++) { parameters.push([pilotID, null, languagesSpoken[j]]); }
+        for (var j = 0; j < languagesSpoken.length; j++) { parameters.push([pilotID, languagesSpoken[j]]); }
 
-        for (var k = 0; k < parameters.length; k++) {
-            MySQL_connection.query(languageQuery, parameters[k], (error, results, fields) => {
-                if (error) {
-                    console.error('Error executing query:', error);
-                    return;
-                }
-                console.log('Rows affected:', results.affectedRows);
-            });
-        }
+
 
         // Add Pilot Instance to MongoDB
-        const pilotToAdd = new PilotClass(pilotID, name, age, gender, sen_level, veh_rest, nationality, maxRange, email);
-        insertPilot(pilotToAdd);
+        // const pilotToAdd = new PilotClass(pilotID, name, age, gender, sen_level, veh_rest, nationality, maxRange, email);
+        // insertPilot(pilotToAdd);
         // MongoDB Atlas Pilot Insert Statement
         // Defined in Pilot.js
 
         // Add languages spoken to MongoDB
-        for (var m = 0; m < parameters.length; m++) {
-            insertLanguage(pilotID, null, languagesSpoken[m]);
-            // MongoDB Atlas Language Insert Statement
-            // Defined in Pilot.js
-        }
+        // for (var m = 0; m < parameters.length; m++) {
+        //     insertLanguage(pilotID, null, languagesSpoken[m]);
+        //     // MongoDB Atlas Language Insert Statement
+        //     // Defined in Pilot.js
+        // }
 
     }
+
+    
 }
+console.log(`completedQueries: ${completedQueries}`);
+console.log(`queryToComplete: ${queryToComplete}`);
+
+
