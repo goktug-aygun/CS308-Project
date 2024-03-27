5691a4a8a92f42bf6c1831005f7fe7438be6f220
goktug-aygun <goktug.aygun@sabanciuniv.edu>
2024-03-27 13:41:07 +0300

    Helper Function

--- Backend/helper_functions.js
+++ Backend/helper_functions.js
@@ -60,35 +60,3 @@ function generateID(type)
 }
 
 module.exports = {generateID};
-
-// Below are blueprints for SQL table creation
-
-// CREATE TABLE pilots
-// {
-//     id VARCHAR(8) NOT NULL,
-//     name VARCHAR(8) NOT NULL,
-//     age VARCHAR(8) NOT NULL,
-//     gender Boolean NOT NULL,
-//     nationality VARCHAR(20) NOT NULL,
-//     // known languages will be given in another table
-//     sen_level VARCHAR(8) NOT NULL,
-//     veh_rest VARCHAR(20) NOT NULL,
-//     max_range integer NOT NULL
-//     PRIMARY KEY (id)
-// }
-
-// CREATE TABLE pilotLanguages
-// {
-//     id VARCHAR(8) NOT NULL,
-//     language VARCHAR(20) NOTNULL
-//     FOREIGN KEY id REFERENCES pilots(id)
-//     PRIMARY KEY (id, language)
-// }
-
-// CREATE TABLE crewLanguages
-// {
-//     id VARCHAR(8) NOT NULL,
-//     language VARCHAR(20) NOTNULL
-//     FOREIGN KEY id REFERENCES crewMembers(id)
-//     PRIMARY KEY (id, language)
-// }
